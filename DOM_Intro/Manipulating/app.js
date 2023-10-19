f48f3093db67edc788b9da8a2ce6a5d152a48653
Mohammed Aldura <mgaldura@umich.edu>
2023-10-18 21:27:45 -0400

    DOM Intro

---
+++ DOM_Intro/Manipulating/app.js
@@ -0,0 +1,14 @@
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!!'
+// }
+
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(7, 255, 81)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy'
+}
+
+
